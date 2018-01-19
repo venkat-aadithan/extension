49b683de89043e928f2e9d3f9d1b65900e9c9773
Venkatesan R <venkatesanr@yoursefficiently.com>
2018-01-19 12:59:32 +0530

    added script files

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
     var newURL = "http://www.irctc.co.in/";
-    chrome.tabs.create({ url: newURL },function(){
-    	//alert('tab created');
-    	document.getElementById('usernameId').value = 'new@gmail.com';
-    	console.log(uname);
+    chrome.tabs.create({ url: newURL,"selected":true },function(tab){
+    	chrome.tabs.executeScript(tab.id, {file: "myscript.js"});
     })
     	
     
---
+++ myscript.js
@@ -0,0 +1,3 @@
+document.getElementsByName('j_username')[0].value = 'arul654321';
+document.getElementsByName('j_password')[0].value = 'Arul654321';
+document.getElementById('nlpAnswer').focus();
