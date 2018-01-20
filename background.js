9a83f5dd459d35940da337dab29b04da4ef89f97
Venkatesan R <venkatesanr@yoursefficiently.com>
2018-01-20 12:42:28 +0530

    jquery added

--- background.js
+++ background.js
@@ -3,7 +3,6 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
     var newURL = "http://www.irctc.co.in/";
     chrome.tabs.create({ url: newURL,"selected":true },function(tab){
     	chrome.tabs.executeScript(tab.id, {file: "myscript.js"});
-    })
+    });
     	
-    
 });
--- myscript.js
+++ myscript.js
@@ -1,3 +1,18 @@
-document.getElementsByName('j_username')[0].value = 'arul654321';
-document.getElementsByName('j_password')[0].value = 'Arul654321';
-document.getElementById('nlpAnswer').focus();
+$(document).ready(function(){
+	
+	$('#usernameId').val('arul654321');
+	$('input[name="j_password"]').val('Arul654321');
+	$('input[name="j_captcha"]').focus();
+	$('input[name="j_captcha"]').keyup(function(){
+		if($(this).val().length === 5){
+			$('#loginbutton').trigger('click');
+		}
+	});
+
+		//alert();
+	$('input[name="jpform:fromStation"]').val('CHENNAI EGMORE - MS');
+	$('input[name="jpform:toStation"]').val('MADURAI JN - MDU');
+	$('input[name="jpform:journeyDateInputDate"]').val('21-01-2018');
+	$('input[name="jpform:jpsubmit"]').trigger('click');
+	
+});
