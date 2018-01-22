e00e8940fe2b689d913565ca26dc089850528022
Venkatesan R <venkatesanr@yoursefficiently.com>
2018-01-22 19:16:51 +0530

    script changes

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-	$('#usernameId').val('*********');
-	$('input[name="j_password"]').val('********');
+	$('#usernameId').val('arul654321');
+	$('input[name="j_password"]').val('Arul654321');
 	$('input[name="j_captcha"]').focus();
 	$('input[name="j_captcha"]').keyup(function(){
 		if($(this).val().length === 5){
@@ -10,9 +10,14 @@ $(document).ready(function(){
 	});
 
 		//alert();
-	$('input[name="jpform:fromStation"]').val('CHENNAI EGMORE - MS');
-	$('input[name="jpform:toStation"]').val('MADURAI JN - MDU');
-	$('input[name="jpform:journeyDateInputDate"]').val('21-01-2018');
-	$('input[name="jpform:jpsubmit"]').trigger('click');
+	if($('input[name="jpform:fromStation"]').val() === ''){
+		$('input[name="jpform:fromStation"]').val('CHENNAI EGMORE - MS');
+		$('input[name="jpform:toStation"]').val('MADURAI JN - MDU');
+		$('input[name="jpform:journeyDateInputDate"]').val('21-01-2018');
+		$('input[name="jpform:jpsubmit"]').trigger('click');
+	}	
+	
+
+	$('.journeyTrainList').children('')
 	
 });
